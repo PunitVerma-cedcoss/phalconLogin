953bdbd741ea502283de6b778013add5964dea19
= <=>
2022-03-23 12:33:36 +0530

    login added

---
+++ src/app/controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+
+class AuthController extends Controller
+{
+    public function indexAction()
+    {
+
+
+        // return '<h1>Hello World!</h1>';
+    }
+}
